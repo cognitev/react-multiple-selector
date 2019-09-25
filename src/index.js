caa81ddcc644afd3fa5093a74c683e6aebf17f91
zeyadetman <zeyad.etman@cognitev.com>
2019-09-25 13:25:33 +0200

    Update index entry

--- src/index.js
+++ src/index.js
@@ -1,27 +1,81 @@
-import React, { useState } from "react";
-import ReactDOM from "react-dom";
-import SelectCities from "./SelectCities";
-import "./styles.css";
+import React, { useState, useEffect } from "react";
+import AsyncSelect from "react-select/async";
+import { countries } from "./data";
+import ListItem from "./ListItem";
+import styles from "./styles.module.scss";
 
-function App() {
-  const [inputValue, setValue] = useState("");
-  return (
-    <div className="react-select-cities">
-      <SelectCities
-        maxCountriesNumber={5}
-        onInputChange={value => {
-          const inputValue = value.replace(/\W/g, "");
-          setValue(inputValue);
-          return inputValue;
-        }}
-        inputValue={inputValue}
-        onChange={(a, b) => {
-          console.log("from Parent: ", a, b);
+const getLastElement = a => a[a.length - 1];
+const filterColors = inputValue =>
+  countries.filter(i =>
+    i.name.toLowerCase().includes(inputValue.toLowerCase())
+  );
+
+const promiseOptions = inputValue =>
+  new Promise(resolve => {
+    setTimeout(() => {
+      resolve(filterColors(inputValue));
+    }, 1000);
+  });
+
+const SelectCities = ({
+  onChange,
+  inputValue,
+  onInputChange,
+  maxCountriesNumber
+}) => {
+  const [currentSelectedValues, selectValues] = useState([]);
+
+  const renderCountriesList = () =>
+    currentSelectedValues.map(country => (
+      <ListItem
+        {...country}
+        onRemoveItem={id => {
+          const newItems = currentSelectedValues.filter(
+            country => country._id !== id
+          );
+          selectValues(newItems);
         }}
       />
+    ));
+
+  return (
+    <div className={styles.reactSelectCities}>
+      <AsyncSelect
+        backspaceRemovesValue={false}
+        isClearable={false}
+        styles={{
+          multiValue: () => ({ display: "none" }),
+          multiValueRemove: base => ({ ...base, display: "none" })
+        }}
+        getOptionLabel={country => country.name}
+        getOptionValue={country => country._id}
+        loadOptions={promiseOptions}
+        onInputChange={onInputChange}
+        value={currentSelectedValues}
+        onChange={(currentValues, action) => {
+          const numberOfCountries = currentValues.reduce(
+            (acc, country) => (acc += Number(country.type === "country")),
+            0
+          );
+          if (numberOfCountries > maxCountriesNumber) {
+            const lastElement = getLastElement(currentValues);
+            if (lastElement.type !== "country") {
+              onChange(currentValues, action);
+              selectValues(currentValues);
+            }
+          } else {
+            onChange(currentValues, action);
+            selectValues(currentValues);
+          }
+        }}
+        inputValue={inputValue}
+        isMulti
+      />
+      <ul className={styles.list}>
+        {currentSelectedValues && renderCountriesList()}
+      </ul>
     </div>
   );
-}
+};
 
-const rootElement = document.getElementById("root");
-ReactDOM.render(<App />, rootElement);
+export default SelectCities;
